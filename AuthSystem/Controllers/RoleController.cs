599cf2d89e65656512a2519ebf8d77894776457a
wahid <hasanalwahid@gmail.com>
2021-07-01 11:19:43 +0600

    Add project files.

---
+++ AuthSystem/Controllers/RoleController.cs
@@ -0,0 +1,46 @@
+﻿using AuthSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProjectRole role)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(role.RoleName);
+            if(!roleExist)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(role.RoleName));
+                //if (result.Succeeded)
+                //{
+                //    return RedirectToAction("Index");
+                //}
+            }
+            return View();
+        }
+    }
+}
